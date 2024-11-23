8682dfd6a14539e5707074ebe1149d699063dfac
permalik <permalik@protonmail.com>
2024-11-22 20:55:41 -0600

    feat: update read file

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 	"os"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
 	f, err := os.Open("/Users/tymalik/Documents/git/markdown_parser_go/test.md")
 	check(err)
 
 	reader := bufio.NewReader(f)
-	data := make([]byte, 100)
-	_, err = reader.Read(data)
+	data, err := reader.ReadBytes('\n')
 	check(err)
 
 	fmt.Println(string(data))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
