ddfa2ee6f3171f2e3f6d2cc316bce7b9e5d2eaeb
permalik <permalik@protonmail.com>
2024-11-23 19:47:18 -0600

    feat: impl readFile with scanner

--- main.go
+++ main.go
@@ -16,38 +16,55 @@ type Token struct {
 func main() {
 	f, e := os.Open("/Users/tymalik/Documents/git/markdown_parser_go/test.md")
 	check(e)
-
 	r := bufio.NewReader(f)
+
+	ll := make([][]byte, 0)
 	lc := 0
-	lex(r, lc)
+	readFile(r, &ll, lc)
+
+	for i, v := range ll {
+		fmt.Printf("line#:%d\n%s\n", i, string(v))
+	}
 }
 
-func lex(r *bufio.Reader, lc int) {
-	for {
-		var l []byte
-		for {
-			d, i, e := r.ReadLine()
-			if e != nil {
-				if e.Error() == "EOF" {
-					break
-				}
-				fmt.Println("error reading file: ", e)
-				return
-			}
-
-			l = append(l, d...)
-
-			if !i {
-				break
-			}
-		}
-		if len(l) == 0 {
-			break
+func readFile(r *bufio.Reader, ll *[][]byte, lc int) {
+	fmt.Println("log")
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		lc += 1
+		if len(s.Bytes()) == 0 {
+			b := []byte{'<', ' ', 'n', 'e', 'w', 'l', 'i', 'n', 'e', ' ', '>'}
+			*ll = append(*ll, b)
 		} else {
-			lc += 1
+			*ll = append(*ll, s.Bytes())
 		}
-		fmt.Printf("line#:%d\n%s\n", lc, string(l))
 	}
+	// for {
+	// 	var l []byte
+	// 	for {
+	// 		d, i, e := r.ReadLine()
+	// 		if e != nil {
+	// 			if e.Error() == "EOF" {
+	// 				break
+	// 			}
+	// 			fmt.Println("error reading file: ", e)
+	// 			return
+	// 		}
+	//
+	// 		l = append(l, d...)
+	//
+	// 		if !i {
+	// 			break
+	// 		}
+	// 	}
+	// 	if len(l) == 0 {
+	// 		n := []byte{'\n'}
+	// 		*ll = append(*ll, n)
+	// 	} else {
+	// 		lc += 1
+	// 		*ll = append(*ll, l)
+	// 	}
+	// }
 }
 
 func check(e error) {
