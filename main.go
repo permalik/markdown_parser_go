5dd76d480afae4a55b1deab0f41076fa09ccac89
permalik <permalik@protonmail.com>
2024-12-07 12:23:13 -0600

    refactor: condense parser and visitor

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/permalik/markdown_parser_go/lex"
-	"github.com/permalik/markdown_parser_go/literal"
+	"github.com/permalik/markdown_parser_go/parse"
 )
 
 func main() {
@@ -44,7 +44,7 @@ func main() {
 	}
 
 	lexer := lex.NewLexer(input, true)
-	parser := NewParser(lexer)
+	parser := parse.NewParser(lexer)
 
 	ast, err := parser.Parse()
 	if err != nil {
@@ -52,7 +52,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	var visitor Visitor
+	var visitor parse.Visitor
 	switch *format_flag {
 	case "md":
 		visitor = NewMDGen(output)
@@ -64,93 +64,6 @@ func main() {
 	ast.Accept(visitor)
 }
 
-type Parser struct {
-	lexer *lex.Lexer
-}
-
-type Node interface {
-	Accept(v Visitor)
-}
-
-type TreeNode struct {
-	Children []Node
-}
-
-type HorizontalRuleHyphenNode struct {
-	Text string
-}
-
-type ListNode struct {
-	Items []string
-}
-
-type ParagraphNode struct {
-	Text string
-}
-
-type Visitor interface {
-	VisitTree(n *TreeNode)
-	VisitHorizontalRuleHyphen(n *HorizontalRuleHyphenNode)
-	VisitList(n *ListNode)
-	VisitParagraph(n *ParagraphNode)
-}
-
-func (n *TreeNode) Accept(v Visitor)                 { v.VisitTree(n) }
-func (n *HorizontalRuleHyphenNode) Accept(v Visitor) { v.VisitHorizontalRuleHyphen(n) }
-func (n *ListNode) Accept(v Visitor)                 { v.VisitList(n) }
-func (n *ParagraphNode) Accept(v Visitor)            { v.VisitParagraph(n) }
-
-func NewParser(lexer *lex.Lexer) *Parser {
-	return &Parser{lexer: lexer}
-}
-
-func (p *Parser) Parse() (Node, error) {
-	tree := &TreeNode{}
-	var currList []string
-
-	for {
-		token, err := p.lexer.NextToken()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, fmt.Errorf("error parsing: %w", err)
-		}
-
-		switch tok := token.Literal.(type) {
-		case literal.HorizontalRuleHyphen:
-			if len(currList) > 0 {
-				tree.Children = append(tree.Children, &ListNode{Items: currList})
-				currList = nil
-			}
-			tree.Children = append(tree.Children, &HorizontalRuleHyphenNode{
-				Text: tok.Text,
-			})
-		case literal.Paragraph:
-			if len(currList) > 0 {
-				tree.Children = append(tree.Children, &ListNode{Items: currList})
-				currList = nil
-			}
-			tree.Children = append(tree.Children, &ParagraphNode{
-				Text: tok.Text,
-			})
-		case literal.ListItem:
-			currList = append(currList, tok.Text)
-		case literal.BlankLine:
-			if len(currList) > 0 {
-				tree.Children = append(tree.Children, &ListNode{Items: currList})
-				currList = nil
-			}
-		}
-	}
-
-	if len(currList) > 0 {
-		tree.Children = append(tree.Children, &ListNode{Items: currList})
-	}
-
-	return tree, nil
-}
-
 type MDGen struct {
 	writer io.Writer
 }
@@ -159,23 +72,23 @@ func NewMDGen(writer io.Writer) *MDGen {
 	return &MDGen{writer: writer}
 }
 
-func (g *MDGen) VisitTree(n *TreeNode) {
+func (g *MDGen) VisitTree(n *parse.TreeNode) {
 	for _, child := range n.Children {
 		child.Accept(g)
 	}
 }
 
-func (g *MDGen) VisitHorizontalRuleHyphen(n *HorizontalRuleHyphenNode) {
+func (g *MDGen) VisitHorizontalRuleHyphen(n *parse.HorizontalRuleHyphenNode) {
 	fmt.Fprintf(g.writer, "%s\n", n.Text)
 }
 
-func (g *MDGen) VisitList(n *ListNode) {
+func (g *MDGen) VisitList(n *parse.ListNode) {
 	for _, item := range n.Items {
 		fmt.Fprintf(g.writer, "* %s\n", item)
 	}
 	fmt.Fprintln(g.writer)
 }
 
-func (g *MDGen) VisitParagraph(n *ParagraphNode) {
+func (g *MDGen) VisitParagraph(n *parse.ParagraphNode) {
 	fmt.Fprintf(g.writer, "%s\n\n", n.Text)
 }
---
+++ parse/parser.go
@@ -0,0 +1,96 @@
+package parse
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/permalik/markdown_parser_go/lex"
+	"github.com/permalik/markdown_parser_go/literal"
+)
+
+type Parser struct {
+	lexer *lex.Lexer
+}
+
+type Node interface {
+	Accept(v Visitor)
+}
+
+type TreeNode struct {
+	Children []Node
+}
+
+type HorizontalRuleHyphenNode struct {
+	Text string
+}
+
+type ListNode struct {
+	Items []string
+}
+
+type ParagraphNode struct {
+	Text string
+}
+
+type Visitor interface {
+	VisitTree(n *TreeNode)
+	VisitHorizontalRuleHyphen(n *HorizontalRuleHyphenNode)
+	VisitList(n *ListNode)
+	VisitParagraph(n *ParagraphNode)
+}
+
+func (n *TreeNode) Accept(v Visitor)                 { v.VisitTree(n) }
+func (n *HorizontalRuleHyphenNode) Accept(v Visitor) { v.VisitHorizontalRuleHyphen(n) }
+func (n *ListNode) Accept(v Visitor)                 { v.VisitList(n) }
+func (n *ParagraphNode) Accept(v Visitor)            { v.VisitParagraph(n) }
+
+func NewParser(lexer *lex.Lexer) *Parser {
+	return &Parser{lexer: lexer}
+}
+
+func (p *Parser) Parse() (Node, error) {
+	tree := &TreeNode{}
+	var currList []string
+
+	for {
+		token, err := p.lexer.NextToken()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error parsing: %w", err)
+		}
+
+		switch tok := token.Literal.(type) {
+		case literal.HorizontalRuleHyphen:
+			if len(currList) > 0 {
+				tree.Children = append(tree.Children, &ListNode{Items: currList})
+				currList = nil
+			}
+			tree.Children = append(tree.Children, &HorizontalRuleHyphenNode{
+				Text: tok.Text,
+			})
+		case literal.Paragraph:
+			if len(currList) > 0 {
+				tree.Children = append(tree.Children, &ListNode{Items: currList})
+				currList = nil
+			}
+			tree.Children = append(tree.Children, &ParagraphNode{
+				Text: tok.Text,
+			})
+		case literal.ListItem:
+			currList = append(currList, tok.Text)
+		case literal.BlankLine:
+			if len(currList) > 0 {
+				tree.Children = append(tree.Children, &ListNode{Items: currList})
+				currList = nil
+			}
+		}
+	}
+
+	if len(currList) > 0 {
+		tree.Children = append(tree.Children, &ListNode{Items: currList})
+	}
+
+	return tree, nil
+}
