bc9a73c64f986b05a2f8914f4eebb6ad0dd522d9
permalik <permalik@protonmail.com>
2024-12-07 12:34:56 -0600

    chore: move elements doc

--- literal/markdown_literal.go
+++ literal/markdown_literal.go
@@ -1,5 +1,37 @@
 package literal
 
+/*
+## Block
+- [ ] Unordered Lists (\*, +, -)
+- [ ] Ordered Lists (1., 2., 3.)
+- [ ] Task Lists (- [ ] Task item)
+- [ ] Definition Lists (Term, followed by a new line : Definition)
+- [ ] Blockquotes (>)
+- [ ] Code Blocks (triple-backspace, four-space indentation)
+- [ ] Tables (using | for columns in a table row)
+
+## Line
+- [ ] Heading (#, ##, ###, ####, #####, ######)
+- [ ] Horizontal Rules (triple- hyphen, underscore, or asterisk)
+- [ ] Footnotes ([^1]: Definition text)
+
+## Inline
+- [ ] Bold (**bold**, __bold__)
+- [ ] Italic (*italic*, _italic_)
+- [ ] Bold and Italic (***bold italic***)
+- [ ] Inline Code (`code`)
+- [ ] Links ([link text](URL "optional title"))
+- [ ] Images (![alt text](URL "optional title"))
+- [ ] Strikethrough (~~strikethrough~~)
+- [ ] Footnote References ([^1] Inline Text)
+- [ ] HTML Elements (<div>content</div>)
+- [ ] Comments (<!-- comment -->)
+
+## Separators
+- [ ] Line Breaks (two or more spaces at the end of a line)
+- [ ] Paragraph (line-separated text)
+*/
+
 type MarkdownLiteral interface {
 	isLiteral()
 }
